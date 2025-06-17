e68f7f79c725cc4034edc3aa0a9578e660b5528d
Muhammad Danial <danialdev95@gmail.com>
2025-06-18 07:23:07 +0800

    Update port

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use("/api/orders", orderRoutes); // Assuming you have an orders route file
 app.use("/api/customers", customerRoutes); // Assuming you have a customers route file
 app.use("/api/products", productRoutes); // Assuming you have a products route file
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Test DB connection before starting
 sequelize
